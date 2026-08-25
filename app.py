a41c9fa9fa70da21c87735dedd8f74f9f5039c46
Fyanagui7 <fabriciogeraldo1976@gmail.com>
2026-08-24 23:30:08 -0300

    feat(soma) soma de 2 + 2

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+    int a =2
+    int b =2
+    result = (a+b)
+    return result
 
 
 if __name__ == '__main__':
